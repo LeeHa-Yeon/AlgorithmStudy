3b30f9537250babf57a1bf02272219ff073d06df
Lee Ha Yeon <gkdus1613@icloud.com>
2020-12-05 17:18:46 +0900

    Commit jeju 9번 문제

---
+++ JejuAlgorithm/ex9.py
@@ -0,0 +1,12 @@
+# 문제9 : sep과 end를 활용한 출력방법
+# 출력 : 2019/04/26 11:34:27
+
+year = '2019'
+month = '04'
+day = '26'
+hour = '11'
+minute = '34'
+second = '27'
+print(year,month, day, sep ='/', end = ' ')
+print(hour, minute, second , sep = ':')
+
