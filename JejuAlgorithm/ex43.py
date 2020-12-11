05544a5db697c1c2913b5685ebddbc925609fc89
Lee Ha Yeon <gkdus1613@icloud.com>
2020-12-12 00:46:15 +0900

    Commit2 jeju 43번 문제

--- JejuAlgorithm/ex43.py
+++ JejuAlgorithm/ex43.py
@@ -42,6 +42,46 @@ print("2진수 :",b2, end="\n")
 print("8진수 :",o2, end="\n")
 print("16진수 :",h2, end="\n")
 
+print("--------> 방법 4: format() 내장함수 사용2 (#빼고)")
+
+num2 = 13
+b2 = format(num2,'b')
+o2 = format(num2,'o')
+h2 = format(num2,'x')
+
+print("2진수 :",b2, end="\n")
+print("8진수 :",o2, end="\n")
+print("16진수 :",h2, end="\n")
+
+print("--------> 방법 5: 문자열.format()를 사용한 진수 변환")
+
+num3 = 13
+b2 = "{0:#b}".format(num3)
+o2 = "{0:#o}".format(num3)
+h2 = "{0:#x}".format(num3)
+d2 = "{0:#d}".format(num3)
+
+print("2진수 :",b2, end="\n")
+print("8진수 :",o2, end="\n")
+print("16진수 :",h2, end="\n")
+print("10진수 :",d2, end="\n")
+
+print("--------> 방법 6: 문자열.format()를 사용한 진수 변환2(#빼고)")
+
+num3 = 13
+b2 = "{0:b}".format(num3)
+o2 = "{0:o}".format(num3)
+h2 = "{0:x}".format(num3)
+d2 = "{0:d}".format(num3)
+
+print("2진수 :",b2, end="\n")
+print("8진수 :",o2, end="\n")
+print("16진수 :",h2, end="\n")
+print("10진수 :",d2, end="\n")
+
+
+
+
 
 
 
