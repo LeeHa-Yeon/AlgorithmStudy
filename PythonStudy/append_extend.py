5141927413855610179e227eca43c4fdbf36a509
Lee Ha Yeon <gkdus1613@icloud.com>
2020-12-13 22:32:41 +0900

    append/extend 차이

--- PythonStudy/append_extend.py
+++ PythonStudy/append_extend.py
@@ -1,31 +1,39 @@
 x = ['Tick', 'Tock', 'Song']
 y = ['Ping', 'Pong']
 x.append(y)
-print('x:', x)
+print('append x:', x)
 
 x = ['Tick', 'Tock', 'Song']
 y = ['Ping', 'Pong']
 x.extend(y)
-print('x:', x)
+print('extend x:', x)
 
+# append x: ['Tick', 'Tock', 'Song', ['Ping', 'Pong']]
+# extend x: ['Tick', 'Tock', 'Song', 'Ping', 'Pong']
 
 x = ['Tick', 'Tock', 'Song']
 y = [['Ping', 'Pong']]
 x.append(y)
-print('x:', x)
+print('append x:', x)
 
 x = ['Tick', 'Tock', 'Song']
 y = [['Ping', 'Pong']]
 x.extend(y)
-print('x:', x)
+print('extend x:', x)
+
+# append x: ['Tick', 'Tock', 'Song', [['Ping', 'Pong']]]
+# extend x: ['Tick', 'Tock', 'Song', ['Ping', 'Pong']]
 
 
 x = ['Tick', 'Tock', 'Song']
 y = 'Ping'
 x.append(y)
-print('x:', x)
+print('append x:', x)
 
 x = ['Tick', 'Tock', 'Song']
 y = 'Ping'
 x.extend(y)
-print('x:', x)
+print('extend x:', x)
+
+# append x: ['Tick', 'Tock', 'Song', 'Ping']
+# extend x: ['Tick', 'Tock', 'Song', 'P', 'i', 'n', 'g']
