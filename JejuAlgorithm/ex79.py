eae6af34273322391358b0d4393c8becb9124bee
Lee Ha Yeon <gkdus1613@icloud.com>
2020-12-16 23:55:02 +0900

    Commit jeju 79 문제 순회 리스트

---
+++ JejuAlgorithm/ex79.py
@@ -0,0 +1,25 @@
+# 문제 79 : 순회하는 리스트
+
+def circuitList(list,n) :
+    # 리스트 얕은 복사
+    l = list[:]
+    for i in range(n) :
+        l.insert(0,l[-1])
+        del l[-1]
+    return l
+
+def distance(a,b) :
+    disList = []
+    for i in range(len(a)) :
+        disList.append(abs(a[i]-b[i]))
+    return disList
+
+n = 2
+lBefore = [10,20,25,27,34,35,39]
+lAfter = circuitList(lBefore,n)
+lminus = distance(lBefore,lAfter)
+
+idx = lminus.index(min(lminus))
+
+print("인덱스 : ",idx)
+print("값 : " , lBefore[idx],lAfter[idx])
