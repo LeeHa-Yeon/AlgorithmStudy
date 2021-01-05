2a8b024bae6317ab5d4a34566a6dd0dd77a6178f
Lee Ha Yeon <gkdus1613@icloud.com>
2021-01-06 01:09:28 +0900

    BackJun 탐욕/ATM/실3

---
+++ BeakJun/Greedy/BJ_11399_ATM.py
@@ -0,0 +1,20 @@
+n = int(input())
+pList = list(map(int,input().split(' ')))  # 3 1 4 3 2  ->   2 5 1 4 3
+wList = pList[:]
+a = sorted(pList) # 1 2 3 3 4
+answerList = []
+answer = 0
+result = 0
+
+for i in range(n) :
+    minIdx = pList.index(a[i])
+    pList[minIdx] = 0
+    answerList.append(minIdx+1)
+
+for j in range(n) :
+    idx = answerList[j]
+    b = wList[idx-1]
+    answer+=b
+    result+=answer
+
+print(result)
