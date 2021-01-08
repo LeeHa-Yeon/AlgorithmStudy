7a962427139d7ec1e33f58df8e58c7307e257733
Lee Ha Yeon <gkdus1613@icloud.com>
2021-01-09 02:21:34 +0900

    PG 탐욕/조이스틱/레2

--- Programmers/Greedy_조이스틱.py
+++ Programmers/Greedy_조이스틱.py
@@ -1,21 +1,48 @@
 def solution(name):
 
+    AlphaUpDownCnt = list()
     answer  = 0
 
-    l = ["A" for _ in range(len(name))]
+    # Alphabet up/down
+    for word in name :
+        AlphaUpDownCnt.append(min(ord(word)-ord('A'),26-(ord(word)-ord('A'))))
+    answer+=sum(AlphaUpDownCnt)
 
-    for k in range(len(name)) :
-        i = name[k]
-        j = l[k]
-        if ord(i) == 65 :
-            answer-=1
-            continue
-        if ord(i) <= 77 :
-            answer+= ord(i)-ord(j)
-            #print('test2',answer)
-        else :
-            answer+= 26-(ord(i)-ord(j))
-            #print('test3',answer)
-    return answer+len(name)-1
+    # Alphabet left/right
+    idx = 0
+    AlphaUpLeftRight = AlphaUpDownCnt[:]
 
-print(solution("JEROEN"))
+    while True :
+        AlphaUpLeftRight[idx] = 0
+        left,right = 1,1
+        if sum(AlphaUpLeftRight) == 0 :
+            return answer
+        # print(AlphaUpLeftRight,AlphaUpLeftRight[idx+right])
+        while AlphaUpLeftRight[idx-left] == 0 :
+            left+=1
+        while AlphaUpLeftRight[idx+right] == 0 :
+            right+=1
+        answer+=left if left<right else right
+        idx+= -left if left<right else right
+        # print(left,right,idx)
+
+print(solution("BBBAAAB"))
+
+'''
+테스트 케이스
+
+print(solution(BBBAAAB)) #8
+print(solution(ABABAAAAABA)) #10
+print(solution(CANAAAAANAN)) #48
+print(solution(ABAAAAABAB)) #8
+print(solution(ABABAAAAAB)) #8
+print(solution(BABAAAAB)) #7
+print(solution(AAA)) #0
+print(solution(ABAAAAAAABA)) #6
+print(solution(AAB)) #2
+print(solution(AABAAAAAAABBB)) #11
+print(solution(ZZZ)) #5
+print(solution(BBBBAAAAAB)) #10
+print(solution(BBBBAAAABA)) #12
+
+'''
