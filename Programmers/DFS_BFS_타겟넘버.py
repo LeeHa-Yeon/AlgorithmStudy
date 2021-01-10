61b49a62e412e92a1c47548518092af9a5a5a3fd
Lee Ha Yeon <gkdus1613@icloud.com>
2021-01-10 13:48:02 +0900

    PG DFS/타겟넘버/레2

---
+++ Programmers/DFS_BFS_타겟넘버.py
@@ -0,0 +1,17 @@
+'''
+numbers	         target	   return
+[1, 2, 3, 4, 5]	 3	       5
+'''
+
+def solution(numbers, target):
+    return dfs(numbers,target,0,0)
+
+
+def dfs(numbers,target,index,num) :
+    if index == len(numbers) :
+        return 1 if num == target else 0
+    return dfs(numbers,target,index+1,num+numbers[index]) + dfs(numbers,target,index+1,num-numbers[index])
+
+
+
+print(solution([1,1,1,1,1],3))
