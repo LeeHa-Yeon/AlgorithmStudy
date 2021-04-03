e5a9c872b3c1b9665da988d59ecac0157f4b2d6d
Lee Ha Yeon <gkdus1613@icloud.com>
2021-04-03 20:19:09 +0900

    Stack_Queue_기능개발

---
+++ re_Programmers/Stack_Queue_기능개발.py
@@ -0,0 +1,34 @@
+# progresses	speeds	return
+# [93, 30, 55]	[1, 30, 5]	[2, 1]
+# [95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+
+
+
+from collections import deque
+import math
+
+def solution(progresses, speeds):
+    answer = []
+    completeDays = deque()
+
+    for i in range(len(progresses)) :
+        completeDays.append(math.ceil((100 - progresses[i]) / speeds[i]))
+
+
+    while completeDays :
+        standard = completeDays.popleft()
+        cnt = 1
+        for day in completeDays:
+            if standard >= day:
+                cnt += 1
+            else:
+                break
+
+        for _ in range(cnt-1):
+            completeDays.popleft()
+
+        answer.append(cnt)
+
+    return answer
+
+print(solution([93, 30, 55],[1, 30, 5]))
