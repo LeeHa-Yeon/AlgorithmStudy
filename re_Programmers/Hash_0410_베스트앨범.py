175e231a5726a471703efdb6bb16d866013136bd
Lee Ha Yeon <gkdus1613@icloud.com>
2021-04-12 02:37:50 +0900

    Hash_0410_베스트앨범 수정

--- re_Programmers/Hash_0410_베스트앨범.py
+++ re_Programmers/Hash_0410_베스트앨범.py
@@ -18,43 +18,43 @@ def solution(genres, plays):
 
     manyPlaySort = sorted(initDict.items(), key = lambda x:x[1], reverse=True)
 
+    # ['A', 'A', 'B', 'A'], [2, 2, 2, 3]
     # 장르 내에서 많이 재생된 노래
     manyGenres = [(genres[i], plays[i]) for i in range(len(genres))]
     manyGenresSort = sorted(manyGenres, key=lambda x: (x[0], -x[1]))
 
     while manyPlaySort :
-        standard = manyPlaySort.pop(0)
+        standard = manyPlaySort.pop(0) # A, B
 
         for i in range(len(manyGenresSort)) :
             if standard[0] == manyGenresSort[i][0] and max !=2  :
                 idx = plays.index(manyGenresSort[i][1])
                 albums.append(idx)
                 plays[idx] = 0
+                print(plays)
                 max+=1
+
         max = 0
 
 
     return albums
 
-print(solution(['A', 'B', 'A'], [600, 500, 600]))# == [0, 2, 1])  &&&&&&&&&&&&
-print(solution(['A', 'B', 'C', 'D'], [1, 2, 3, 1]))# == [2, 1, 0, 3])  **********
-print(solution(['A', 'A', 'B', 'A'], [2, 2, 2, 3]) )#== [3, 0, 2]) ******
-print(solution(['A', 'A', 'B', 'A'], [5, 5, 6, 5]) )#== [0, 1, 2]) *******
-print(solution(['A', 'A', 'B', 'A', 'B', 'B'], [5, 5, 6, 5, 7, 7]))# == [4, 5, 0, 1]) ******
-
-
-#
+# print(solution(['A', 'B', 'A'], [600, 500, 600]))#  [0, 2, 1]
+# print(solution(['A', 'B', 'C', 'D'], [1, 2, 3, 1]))#  [2, 1, 0, 3]
+print(solution(['A', 'A', 'B', 'A'], [2, 2, 2, 3]) )# [3, 0, 2] ******
+# print(solution(['A', 'A', 'B', 'A'], [5, 5, 6, 5]) )# [0, 1, 2]
+# print(solution(['A', 'A', 'B', 'A', 'B', 'B'], [5, 5, 6, 5, 7, 7]))#  [4, 5, 0, 1]
 # print(solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500]))
-# print(solution(['B', 'A'], [500, 600])) # [1, 0])
-# print(solution(['B'], [500])) # [0])
-# print(solution(['B', 'A'], [600, 500])) # [0, 1])
-# print(solution(['A', 'B'], [600, 500])) #[0, 1])
-# print(solution(['A', 'A', 'B'], [600, 500, 300]))# == [0, 1, 2])
+# print(solution(['B', 'A'], [500, 600])) # [1, 0]
+# print(solution(['B'], [500])) # [0]
+# print(solution(['B', 'A'], [600, 500])) # [0, 1]
+# print(solution(['A', 'B'], [600, 500])) #[0, 1]
+# print(solution(['A', 'A', 'B'], [600, 500, 300]))#  [0, 1, 2]
 #
-# print(solution(['A', 'B', 'A'], [600, 500, 700]))# == [2, 0, 1])
-# print(solution(['A', 'A', 'A'], [600, 500, 700]))# == [2, 0])
-# print(solution(['A', 'A', 'A'], [3, 2, 1]) )#== [0, 1])
+# print(solution(['A', 'B', 'A'], [600, 500, 700]))#  [2, 0, 1]
+# print(solution(['A', 'A', 'A'], [600, 500, 700]))#  [2, 0]
+# print(solution(['A', 'A', 'A'], [3, 2, 1]) )# [0, 1]
 # print(solution(['classic', 'pop', 'classic', 'classic', 'pop'],
-#                [500, 600, 150, 800, 2500]) )#== [4, 1, 3, 0])
-# print(solution(['classic'], [2500]) )#== [0])
-# print(solution(['A', 'B', 'C'], [1, 2, 3]) )#== [2, 1, 0])
+#                [500, 600, 150, 800, 2500]) )# [4, 1, 3, 0]
+# print(solution(['classic'], [2500]) )# [0]
+# print(solution(['A', 'B', 'C'], [1, 2, 3]) )# [2, 1, 0]
