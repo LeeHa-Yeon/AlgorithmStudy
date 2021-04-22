4ae035e15ccb88d25949ec358fb6fc8400ff36dc
Lee Ha Yeon <gkdus1613@icloud.com>
2021-04-22 18:41:40 +0900

    Greedy_체육복

--- re_Programmers/Greedy_체육복.py
+++ re_Programmers/Greedy_체육복.py
@@ -5,19 +5,20 @@ n	lost	reserve	return
 3	[3]	[1]	2
 '''
 
-# 5, [1, 2, 3], [2, 3, 4]
 def solution(n, lost, reserve):
     student = [ i for i in range(1,n+1)]
     lesson = set(student)-set(lost)
     same = []
 
+    # 여벌이 2개인데 체육복을 도난 당한 사람들 same
     for i in reserve :
         if i in lost :
             same.append(i)
+
     lost = list(set(lost) - set(same))
     reserve = list(set(reserve) - set(same))
 
-
+    # 그 외 남은 사람들 중 여벌 옷을 자기 앞 뒤 사람에게 줄 수 있는 경
     while lost :
         standard = lost.pop(0)  # 1
 
@@ -28,19 +29,5 @@ def solution(n, lost, reserve):
                 reserve.append(40)
 
     return len(lesson)+len(same)
-
-# print(solution(5,	[2, 4],	[1, 3, 5]))
-# print("-----")
-# print(solution(5,	[2, 4]	,[3]))
-# print("-----")
-# print(solution(3,	[3],	[1]))
-# print("-----")
-# print(solution(5,	[3],	[1,3]))
-# print("------")
-# print(solution(2, [2], [1] ))
-# print("------")
-# print(solution(7 ,[1, 2, 3, 4, 5, 6, 7],[1, 2, 3]))
-# print("-----")
-# print(solution(4, [3, 1, 2], [2, 4, 3]))
 
 print(solution(5, [1, 2, 3], [2, 3, 4]))
