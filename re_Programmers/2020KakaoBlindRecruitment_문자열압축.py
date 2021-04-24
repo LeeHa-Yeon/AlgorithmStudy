7124057909b2295974d415e75c4f27c9c7f1090a
Lee Ha Yeon <gkdus1613@icloud.com>
2021-04-24 19:36:37 +0900

    문자열 압축

--- re_Programmers/2020KakaoBlindRecruitment_문자열압축.py
+++ re_Programmers/2020KakaoBlindRecruitment_문자열압축.py
@@ -15,23 +15,80 @@ def solution(s):
         m = n
         cnt = 1
         result = s[j:m]
+        print("n",n)
         while m <= len(s) :
             standard = s[j:m]
             j += n
             m += n
             comparison = s[j:m]
             if standard == comparison :
+                print("같음", standard, comparison)
                 cnt += 1
             else :
+                print("다름",standard,comparison)
                 if cnt == 1 :
                     result += comparison
                 else :
                     result += str(cnt) + comparison
                 cnt = 1
         answer.append(len(result))
+        print('----------')
         n+=1
 
     return min(answer)
 
 print(solution("aabbaccc"))
 
+
+
+''' 한개 비교 햇을 때
+result = s[0]  # 첫번째 값을 결과에 넣는다
+count  = 0   #
+
+for st in s:
+    if st == result[-1]:  #
+        count += 1
+    else:
+        result += str(count) + st
+        count = 1
+result += str(count)
+
+print result
+
+'''
+
+
+''' 여러개 비교 했을 때 
+def solution(s):
+    answer = []
+    n = 1
+    while n <= len(s) :  # n : 짜르는 문자열의 길
+        j = 0
+        m = n
+        cnt = 1
+        result = s[j:m]
+        print("n",n)
+        while m <= len(s) :
+            standard = s[j:m]
+            j += n
+            m += n
+            comparison = s[j:m]
+            if standard == comparison :
+                print("같음", standard, comparison)
+                cnt += 1
+            else :
+                print("다름",standard,comparison)
+                if cnt == 1 :
+                    result += comparison
+                else :
+                    result += str(cnt) + comparison
+                cnt = 1
+        answer.append(len(result))
+        print('----------')
+        n+=1
+
+    return min(answer)
+
+print(solution("aabbaccc"))
+
+'''
