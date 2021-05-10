65f200f8da925a7474d29c5bf3c2c00337c180b1
Lee Ha Yeon <gkdus1613@icloud.com>
2021-05-10 21:38:17 +0900

    2017_팁스타운_예상대진표 - 답 봄

--- re_Programmers/2017_팁스타운_예상대진표.py
+++ re_Programmers/2017_팁스타운_예상대진표.py
@@ -5,54 +5,13 @@ N	A	B	answer
 
 
 def solution(n, a, b):
-    answer = 0
+    next_round = 0
+
     while a != b:
-        answer += 1
-        a = (a + 1) // 2
-        b = (b + 1) // 2
-    return answer
+        a = (a+1) // 2
+        b = (b+1) // 2
+        next_round += 1
 
-print(solution(8,4,5))
+    return next_round
 
-
-'''
-def solution(n,a,b):
-    l = [ i+1 for i in range(n)]
-    participant = []
-    round = 1
-
-    # 초기 참여자 대진표 만들기
-    # [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16]]
-    for i in range(0, len(l), 2):
-        participant.append(l[i:i+2])
-
-        if [a,b] in participant :
-            return round
-
-    win = []
-    winresult =[]
-    cnt = 0
-
-    while [a,b] not in participant and participant :
-        p = participant.pop(0)
-
-        if a in p :
-            win.append(a)
-        elif b in p:
-            win.append(b)
-        else:
-            win.append(p[0])
-        cnt += 1
-        if cnt % 2 == 0:
-            winresult.append(win)
-            win = []
-
-        if len(participant) == 0 :
-            participant = winresult[:]
-            winresult = []
-            win = []
-            round+=1
-
-
-    return round
-'''
+print(solution(8,2,3))
