f0c78e795e026ffa67fc46c68bd391f25dde4d14
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-12 23:05:58 +0900

    괄호변환 백업

--- re_Programmers/2020KakaoBlindRecruitment_괄호변환.py
+++ re_Programmers/2020KakaoBlindRecruitment_괄호변환.py
@@ -3,11 +3,63 @@ p	            result
 "(()())()"	    "(()())()"
 ")("	        "()"
 "()))((()"	    "()(())()"
+
+1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
+2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
+
+3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
+  3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
+4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
+  4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
+  4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
+  4-3. ')'를 다시 붙입니다.
+  4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
+  4-5. 생성된 문자열을 반환합니다.
 '''
+
+def Separation(p) :
+    u, v = [], []
+
+    for i in range(len(p)):
+        if p[:i + 1].count('(') == p[:i + 1].count(')') and p[i + 1:].count('(') == p[i + 1:].count(')'):
+            u = p[:i + 1]
+            v = p[i + 1:]
+            break
+
+    uTest = "".join(u[:])
+
+    while uTest.find('()') != -1:
+        uTest = uTest.replace('()', '')
+
+    # u가 올바른 괄호일 경우
+    if uTest.find(')(') == -1 :
+        if v == '' :
+            return u
+        return u+Separation(v)
+    # 올바르지 못한 괄호일 경우
+    else :
+        binList = ['(']
+        if v != "":
+            binList.append(v)
+        binList.append(')')
+        uTest = uTest[1:len(uTest) - 1]
+        if uTest.find(')') != -1:
+            uTest = uTest.replace(')', '[')
+        if uTest.find('(') != -1:
+            uTest = uTest.replace('(', ')')
+            if uTest.find('[') != -1:
+                uTest = uTest.replace('[', '(')
+        binList.append(uTest)
+        print(binList)
+        return ''.join(binList)
 
 
 def solution(p):
-    answer = ''
-    return answer 
+    if p == '' :
+        return ''
+    return Separation(p)
 
-print(solution("(()())()"))
+# print(solution("(()())()"))
+# print(solution(")("))
+print(solution("()))((()"))
+# print(solution(")()()()("))
