240e69b6acaafeca16dba683d6be9132765a2f45
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-21 17:11:04 +0900

    찾아라_프로그래밍_마에스터_게임맵최단거리

---
+++ re_Programmers/찾아라_프로그래밍_마에스터_게임맵최단거리.py
@@ -0,0 +1,33 @@
+'''
+maps	answer
+[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]	11
+[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]	-1
+'''
+from collections import deque
+def solution(maps):
+    row,col = len(maps),len(maps[1])
+    dx,dy = [-1,0,1,0],[0,1,0,-1] #
+    dis = [[1]*col for _ in range(row)]
+    Q = deque()
+    Q.append((0,0))
+    maps[0][0] = 0
+    while Q :
+        k = Q.popleft()
+        for i in range(4) :
+            # 왼쪽,위,오른쪽,아래
+            x = k[0]+dx[i]
+            y = k[1]+dy[i]
+
+            if 0<=x<row and 0<=y<col and maps[x][y] == 1 :
+                maps[x][y] = 0
+                dis[x][y] = (dis[k[0]][k[1]])+1
+                Q.append((x,y))
+
+    if dis[row-1][col-1] == 1 :
+        return -1
+    else :
+        return dis[row-1][col-1]
+
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
