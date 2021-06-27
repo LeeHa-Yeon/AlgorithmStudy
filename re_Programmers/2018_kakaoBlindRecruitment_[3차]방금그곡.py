4d5363d43dcfca542bad5ee6894617fe9c1def7d
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-27 23:52:28 +0900

    2018_kakaoBlindRecruitment_[3차]방금그곡_몇개 실패

--- re_Programmers/2018_kakaoBlindRecruitment_[3차]방금그곡.py
+++ re_Programmers/2018_kakaoBlindRecruitment_[3차]방금그곡.py
@@ -15,7 +15,10 @@ def solution(m, musicinfos):
         musicTime = int(musicSplit[1][3:])-int(musicSplit[0][3:])
         if mLen > musicTime :
             continue
+        if musicTime < len(musicSplit[3]) :
+            musicSplit[3] = musicSplit[3][:musicTime]
         repeatSheet = "".join(list(chain.from_iterable(repeat(musicSplit[3],len(m)//len(musicSplit[3])+1))))
+        print(repeatSheet)
         if repeatSheet.find(m) != -1 :
             answer[musicTime]=musicSplit[2]
 
@@ -29,7 +32,7 @@ def solution(m, musicinfos):
 
 print(solution("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
 print(solution("CC#BCC#BCC#BCC#B",["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
-print(solution("ABC",["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("ABC",["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCD"]))
 
 '''
 1. 먼저 m의 길이가 음악시간에 포함되는지 확인
