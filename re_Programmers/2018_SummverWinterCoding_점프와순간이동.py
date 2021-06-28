cd93f2b9c187c5208e10ad57ad4b79fb7387e189
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-28 15:53:45 +0900

    2018_SummverWinterCoding_점프와순간이동

--- re_Programmers/2018_SummverWinterCoding_점프와순간이동.py
+++ re_Programmers/2018_SummverWinterCoding_점프와순간이동.py
@@ -9,8 +9,16 @@ k칸 점프 => 건전지 k만큼 닳음
 
 
 def solution(n):
-    ans = 0
-    return ans
+    q,r = divmod(n,2)
+    answer = r
+    while q!=0 :
+        q,r= divmod(q,2)
+        if r == 1 :
+            answer+=1
+    return answer
+
+
 
 print(solution(5))
 print(solution(6))
+print(solution(5000))
