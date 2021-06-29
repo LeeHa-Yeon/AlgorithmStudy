f6351519aa5b430edd42ed7c3a5625cf0a4889f0
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-30 03:09:38 +0900

    월간코드챌린지시즌2_2개이하로다른비트

--- re_Programmers/월간코드챌린지시즌2_2개이하로다른비트.py
+++ re_Programmers/월간코드챌린지시즌2_2개이하로다른비트.py
@@ -23,21 +23,3 @@ def solution(numbers):
 
 print(solution([2,7,21]))
 print(solution([1001,337,0,1,333,673,343,221,898,997,121,1015,665,779,891,421,222,256,512,128,100]))
-
-'''
-def solution(numbers):
-    for num in numbers :
-        print(num)
-        numBinary = bin(num)[2:].zfill(len(bin(num)[2:])+3)
-        for i in range(num+1,num**2) :
-            iBinary = bin(i)[2:].zfill(len(numBinary))
-            print(numBinary)
-            print(iBinary)
-            print(bin(int(numBinary)^int(iBinary))[:])
-
-
-        print("-----")
-    return ""
-
-print(solution([2,7]))
-'''
