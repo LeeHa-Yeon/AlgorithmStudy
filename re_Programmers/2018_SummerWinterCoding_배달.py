d045232b6d5e41d20fc1d7bc3583188bb8850d8b
Lee Ha Yeon <gkdus1613@icloud.com>
2021-07-02 00:59:28 +0900

    2018_SummerWinterCoding_배달

--- re_Programmers/2018_SummerWinterCoding_배달.py
+++ re_Programmers/2018_SummerWinterCoding_배달.py
@@ -5,36 +5,29 @@ N	road	K	result
 '''
 
 import heapq as h
-
 INF = int(1e9)
+def dijkstra(dis,graph):
+    heap = []
+    h.heappush(heap,[0,1])
+
+    while heap :
+        cost,node=h.heappop(heap)
+        for c,n in graph[node] :
+            if cost+c<dis[n] :
+                dis[n]=cost+c
+                h.heappush(heap,[cost+c,n])
 
 def solution(N, road, K):
-    distance = [INF] * (N + 1)
-    q = []
+    dis = [INF]*(N+1)
+    dis[1]=0
+    graph = [[] for _ in range(N+1)]
+    for r in road :
+        graph[r[0]].append([r[2],r[1]])
+        graph[r[1]].append([r[2],r[0]])
+    dijkstra(dis,graph)
+    return len([x for x in dis if x <=K ])
 
-    # 모든 간선 정보를 입력받기
-    road.sort()
-    graph = [[] for _ in range(N + 1)]
-    for i in road:
-        graph[i[0]].append((i[1], i[2]))
 
-    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-    h.heappush(q, (1, 0))
-    distance[1] = 0
-
-    while q:  # 큐가 비어있지 않다면
-        print(q)
-        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        now, dist = h.heappop(q)
-        # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
-            print(i)
-            cost = dist + i[1]
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost > K:
-                distance[i[0]] = cost
-                h.heappush(q, (cost, i[0]))
-    return
 
 print(solution(5, [[1, 2, 1], [2, 3, 3], [5, 2, 2], [1, 4, 2], [5, 3, 1], [5, 4, 2]], 3))
 print(solution(6, [[1, 2, 1], [1, 3, 2], [2, 3, 2], [3, 4, 3], [3, 5, 2], [3, 5, 3], [5, 6, 1]], 4))
