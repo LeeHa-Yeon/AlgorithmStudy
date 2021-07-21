954522186aace2f9f4c7bb42028f08b03c439597
Lee Ha Yeon <gkdus1613@icloud.com>
2021-07-21 12:07:57 +0900

    병합정렬 수정

--- AlgorithmStudy/Sort/MegeSort.py
+++ AlgorithmStudy/Sort/MegeSort.py
@@ -17,16 +17,16 @@ def merge(left, right):
         if len(left) > 0 and len(right) > 0:
             if left[0] <= right[0]:
                 result.append(left[0])
-                left = left[1:]
+                left.pop(0)
             else:
                 result.append(right[0])
-                right = right[1:]
+                right.pop(0)
         elif len(left) > 0:
             result.append(left[0])
-            left = left[1:]
+            left.pop(0)
         elif len(right) > 0:
             result.append(right[0])
-            right = right[1:]
+            right.pop(0)
     return result
 
 print(merge_sort([14, 7, 3, 12, 9, 11, 6, 2]))
