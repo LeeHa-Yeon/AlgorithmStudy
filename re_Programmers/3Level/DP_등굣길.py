05940c680141812a3719b1e542c1b03065ae972b
Lee Ha Yeon <gkdus1613@icloud.com>
2021-08-03 12:39:18 +0900

    DP_등굣길

---
+++ re_Programmers/3Level/DP_등굣길.py
@@ -0,0 +1,20 @@
+def solution(m, n, puddles):
+    answer = [[0 for _ in range(m)] for _ in range(n)]
+    answer[0][0] = 1
+
+    for x in range(n):
+        for y in range(m):
+            if x==y==0 :
+                continue
+            if [x+1,y+1] in puddles:
+                answer[x][y] = 0
+            else:
+                if x == 0:
+                    answer[x][y]+=answer[x][y - 1]
+                elif y == 0:
+                    answer[x][y]+=answer[x - 1][y]
+                else:
+                    answer[x][y]+=(answer[x - 1][y] + answer[x][y - 1])
+    return answer[-1][-1]%1000000007
+
+print(solution(4,3,[[2, 2]]))
