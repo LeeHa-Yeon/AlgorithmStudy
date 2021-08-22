ab5f25cd9bdc68104291c6240b0441f074ca1ad2
Lee Ha Yeon <gkdus1613@icloud.com>
2021-08-22 22:18:58 +0900

    dfs bfs

--- liveCoding/dfs_bfs.py
+++ liveCoding/dfs_bfs.py
@@ -1,11 +1,45 @@
-def DFS(graph, root) :
-    visited = []
-    stack = [root]
+from collections import deque
 
-    while stack :
-        n = stack.pop()
-        if n not in visited :
-            visited.append(n)
-            stack+=graph[n] - set(visited)
+def BFS(graph,start) :
+    visited = []
+    queue = deque(start)
+
+    while queue :
+        node = queue.popleft()
+        if node not in visited :
+            visited.append(node)
+            queue.extend(graph[node])
+
     return visited
 
+def DFS(graph,start) :
+    visited = []
+    stack = [start]
+
+    while stack :
+        node = stack.pop()
+        if node not in visited :
+            visited.append(node)
+            stack.extend(graph[node])
+
+    return visited
+
+if __name__ == "__main__":
+    graph = {
+        'A': ['B'],
+        'B': ['A', 'C', 'H'],
+        'C': ['B', 'D'],
+        'D': ['C', 'E', 'G'],
+        'E': ['D', 'F'],
+        'F': ['E'],
+        'G': ['D'],
+        'H': ['B', 'I', 'J', 'M'],
+        'I': ['H'],
+        'J': ['H', 'K'],
+        'K': ['J', 'L'],
+        'L': ['K'],
+        'M': ['H']
+    }
+
+    print(BFS(graph, 'A'))
+    print(DFS(graph, 'A'))
