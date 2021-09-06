725cd9def67a57afbec7b8656f243ca81d411d02
Lee Ha Yeon <gkdus1613@icloud.com>
2021-09-06 11:03:56 +0900

    피보나치함수_1003

--- BeakJun/DP/피보나치함수_1003.py
+++ BeakJun/DP/피보나치함수_1003.py
@@ -1,5 +1,27 @@
 import sys
 T = int(sys.stdin.readline())
+
+
+for _ in range(T) :
+    zeroCnt = [1, 0]
+    oneCnt = [0, 1]
+
+    N = int(sys.stdin.readline())
+
+    if N == 0 :
+        print("1 0")
+    elif N == 1 :
+        print("0 1")
+    else :
+        for i in range(2,N+1) :
+            zeroCnt.append(zeroCnt[i-1]+zeroCnt[i-2])
+            oneCnt.append(oneCnt[i-1]+oneCnt[i-2])
+        print(zeroCnt.pop(),oneCnt.pop())
+
+
+
+## 메모리 초과 ㅠㅠ
+'''
 nList = []
 
 for _ in range(T) :
@@ -20,3 +42,4 @@ for n in nList :
     oneCnt = l.count('1')
 
     print(zeroCnt,oneCnt)
+'''
