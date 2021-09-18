afaa1cc73eeb1362b487cd4e00ee885bdd3de835
Lee Ha Yeon <gkdus1613@icloud.com>
2021-09-18 23:09:51 +0900

    위클리챌린지_7주차

---
+++ re_Programmers/위클리챌린지_7주차.py
@@ -0,0 +1,20 @@
+'''
+enter	leave	result
+[1,3,2]	[1,2,3]	[0,1,1]
+[1,4,2,3]	[2,1,3,4]	[2,2,1,3]
+[3,2,1]	[2,1,3]	[1,1,2]
+[3,2,1]	[1,3,2]	[2,2,2]
+[1,4,2,3]	[2,1,4,3]	[2,2,0,2]
+'''
+
+def solution(enter, leave):
+    answer = [[] for _ in range(len(enter)+1)]
+    room = []
+
+
+
+
+
+    return answer
+
+print(solution([1,4,2,3],[2,1,3,4]))
