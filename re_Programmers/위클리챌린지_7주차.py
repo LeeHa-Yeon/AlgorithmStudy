5bcd49dfcf00e2b4254af26817171d04dc27c1f8
Lee Ha Yeon <gkdus1613@icloud.com>
2021-09-19 00:31:16 +0900

    위클리챌린지_7주차

--- re_Programmers/위클리챌린지_7주차.py
+++ re_Programmers/위클리챌린지_7주차.py
@@ -8,13 +8,24 @@ enter	leave	result
 '''
 
 def solution(enter, leave):
-    answer = [[] for _ in range(len(enter)+1)]
-    room = []
+    n = len(enter)
+    answer = [0] * (n+1)
+    enter_idx = 0
+    leave_idx = 0
+    room = set()
+    while leave_idx < n:
+        if leave[leave_idx] in room:
+            room.discard(leave[leave_idx])
+            leave_idx += 1
+            continue
+        if enter[enter_idx] not in room:
+            print(room)
+            for man in room:
+                answer[man] += 1
+            answer[enter[enter_idx]] = len(room)
+            room.add(enter[enter_idx])
+            enter_idx += 1
+    return answer[1:]
 
-
-
-
-
-    return answer
 
 print(solution([1,4,2,3],[2,1,3,4]))
